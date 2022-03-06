baf4fec376ee7edb0a13b5a39e8ea41b9e3a2f57
Amjad Alsharafi <amjadsharafi10@gmail.com>
2022-03-06 23:23:05 +0800

    Added initial notification settings layout
    Added the ability to preview a ringtone using `AudioPlayer`.

--- lib/modules/settings/screens/notification.dart
+++ lib/modules/settings/screens/notification.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:iathan/modules/settings/screens/prayer_settings.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class NotificationScreen extends StatefulWidget {
-  NotificationScreen({Key? key}) : super(key: key);
+  const NotificationScreen({Key? key}) : super(key: key);
 
   @override
   State<NotificationScreen> createState() => _NotificationScreenState();
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  AbstractSettingsTile _buildPrayerSettingsTile(String prayer) {
+    return SettingsTile(
+      title: Text(prayer),
+      trailing: const Text("No alarm"),
+      onPressed: (context) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PrayerSettingsScreen(prayer),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notification'),
+        title: const Text('Notification'),
       ),
-      body: Center(
-        child: Text('Notification'),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: SettingsList(
+          sections: [
+            SettingsSection(
+              title: const Text('Prayer notifications'),
+              tiles: [
+                _buildPrayerSettingsTile("Fajr"),
+                _buildPrayerSettingsTile("Sunrise"),
+                _buildPrayerSettingsTile("Dhuhr"),
+                _buildPrayerSettingsTile("Asr"),
+                _buildPrayerSettingsTile("Maghrib"),
+                _buildPrayerSettingsTile("Isha"),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/modules/settings/screens/prayer_settings.dart
@@ -0,0 +1,110 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+class PrayerSettingsScreen extends StatefulWidget {
+  PrayerSettingsScreen(this.prayer, {Key? key}) : super(key: key);
+
+  String prayer;
+
+  @override
+  State<PrayerSettingsScreen> createState() => _PrayerSettingsScreenState();
+}
+
+class _PrayerSettingsScreenState extends State<PrayerSettingsScreen> {
+  // or as a local variable
+  final _audioCache = AudioCache();
+
+  bool _vibrateEnable = false;
+  bool _soundEnable = false;
+
+  int _selectedRington = 0;
+
+  _ringtonTile(String title, int index) {
+    return SettingsTile(
+      title: Text(title),
+      trailing: _selectedRington == index ? const Icon(Icons.check) : null,
+      onPressed: (context) async {
+        setState(() {
+          _selectedRington = index;
+        });
+
+        // TODO: replace with audio file based on title?
+        AudioPlayer player = await _audioCache.play('knock.mp3');
+
+        player.onPlayerCompletion.listen((event) {
+          Navigator.pop(context);
+        });
+
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return TextButton(
+              child: const Text("Stop"),
+              onPressed: () {
+                if (player.state == PlayerState.PLAYING) {
+                  player.stop();
+                  Navigator.pop(context);
+                }
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.prayer),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: SettingsList(
+          sections: [
+            SettingsSection(
+              tiles: [
+                SettingsTile.switchTile(
+                  initialValue: _vibrateEnable,
+                  onToggle: (value) {
+                    setState(() {
+                      _vibrateEnable = value;
+                    });
+                  },
+                  title: const Text('Vibrate'),
+                  leading: Icon(Icons.vibration),
+                ),
+              ],
+            ),
+            SettingsSection(
+              tiles: [
+                SettingsTile.switchTile(
+                  initialValue: _soundEnable,
+                  onToggle: (value) {
+                    setState(() {
+                      _soundEnable = value;
+                    });
+                  },
+                  title: const Text('Sound'),
+                  leading: Icon(Icons.volume_up),
+                ),
+              ],
+            ),
+            if (_soundEnable)
+              SettingsSection(
+                title: const Text('Ringtones'),
+                tiles: [
+                  _ringtonTile("knock, knock", 0),
+                  _ringtonTile("Rington 1", 1),
+                  _ringtonTile("Rington 2", 2),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
