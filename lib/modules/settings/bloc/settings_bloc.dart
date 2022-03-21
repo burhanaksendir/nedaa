cf032e628ad5007e7bacfd5e627007eb0f2d6f2e
Amjad Alsharafi <amjadsharafi10@gmail.com>
2022-03-21 22:17:06 +0800

    Moved accessing `shared_prefs` to `settings_repository`

--- lib/modules/settings/bloc/settings_bloc.dart
+++ lib/modules/settings/bloc/settings_bloc.dart
@@ -3,50 +3,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iathan/modules/settings/repositories/settings_repository.dart';
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
-  static ThemeMode _getThemeFromString(String? theme) {
-    switch (theme) {
-      case 'ThemeMode.light':
-        return ThemeMode.light;
-      case 'ThemeMode.dark':
-        return ThemeMode.dark;
-      case 'ThemeMode.system':
-        return ThemeMode.system;
-      default:
-        return ThemeMode.system;
-    }
-  }
-
-  static Locale _getLocaleFromString(String? locale) {
-    switch (locale) {
-      case 'ar':
-        return const Locale('ar');
-      case 'en':
-        return const Locale('en');
-      default:
-        return const Locale('en');
-    }
-  }
-
   SettingsBloc(this.settingsRepository)
       : super(
           SettingsState(
-              appLanguage: _getLocaleFromString(
-                  settingsRepository.getString("language")),
-              appTheme:
-                  _getThemeFromString(settingsRepository.getString("theme"))),
+              appLanguage: settingsRepository.getLanguage(),
+              appTheme: settingsRepository.getTheme()),
         ) {
     on<LanguageEvent>(
       (event, emit) => {
         emit(SettingsState(
             appLanguage: event.appLanguage, appTheme: state.appTheme)),
-        settingsRepository.setString("language", event.appLanguage.languageCode)
+        settingsRepository.setLanguage(event.appLanguage)
       },
     );
-    on<ThemeEvent>((event, emit) => {
-          emit(SettingsState(
-              appLanguage: state.appLanguage, appTheme: event.theme)),
-          settingsRepository.setString('theme', event.theme.toString())
-        });
+    on<ThemeEvent>(
+      (event, emit) => {
+        emit(SettingsState(
+            appLanguage: state.appLanguage, appTheme: event.theme)),
+        settingsRepository.setTheme(event.theme)
+      },
+    );
   }
 
   final SettingsRepository settingsRepository;
--- lib/modules/settings/repositories/settings_repository.dart
+++ lib/modules/settings/repositories/settings_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsRepository {
@@ -5,11 +6,45 @@ class SettingsRepository {
 
   final SharedPreferences _sharedPref;
 
-  getString(String key) {
+  String? _getString(String key) {
     return _sharedPref.getString(key);
   }
 
-  setString(String key, String value) async {
+  Future<void> _setString(String key, String value) async {
     await _sharedPref.setString(key, value);
+  }
+
+  ThemeMode getTheme() {
+    var theme = _getString('theme');
+    switch (theme) {
+      case 'ThemeMode.light':
+        return ThemeMode.light;
+      case 'ThemeMode.dark':
+        return ThemeMode.dark;
+      case 'ThemeMode.system':
+        return ThemeMode.system;
+      default:
+        return ThemeMode.system;
+    }
+  }
+
+  setTheme(ThemeMode theme) async {
+    await _setString('theme', theme.toString());
+  }
+
+  Locale getLanguage() {
+    var language = _getString('language');
+    switch (language) {
+      case 'en':
+        return const Locale('en');
+      case 'ar':
+        return const Locale('ar');
+      default:
+        return const Locale('en');
+    }
+  }
+
+  setLanguage(Locale language) async {
+    await _setString('language', language.languageCode);
   }
 }
