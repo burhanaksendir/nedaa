53017d5e50f3954666398f115d3c90a8ba531047
Amjad Alsharafi <amjadsharafi10@gmail.com>
2022-03-30 23:19:55 +0800

    Parsed prayer timing data
    Co-authored-by: Fahad <F2had@users.noreply.github.com>

--- lib/modules/prayer_times/models/prayer_times.dart
+++ lib/modules/prayer_times/models/prayer_times.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:nedaa/modules/settings/models/prayer_type.dart';
 
 class DayPrayerTimes {
@@ -5,6 +6,19 @@ class DayPrayerTimes {
   final DateTime date;
   // TODO: add hijri date
 
-
   DayPrayerTimes(this.prayerTimes, this.date);
+
+  factory DayPrayerTimes.fromJson(Map<String, dynamic> json) {
+    var prayerTimes = <PrayerType, DateTime>{};
+
+    Map<String, dynamic> prayerTimesJson = json['timings'];
+    apiNames.forEach((prayerType, prayerName) {
+      prayerTimes[prayerType] =
+          DateTime.parse(prayerTimesJson[prayerName].split(' ')[0]);
+    });
+    return DayPrayerTimes(
+      prayerTimes,
+      DateFormat('dd-MM-yyyy').parse(json['date']['gregorian']['date']),
+    );
+  }
 }
--- lib/utils/services/rest_api_service.dart
+++ lib/utils/services/rest_api_service.dart
@@ -1,20 +1,23 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:nedaa/constants/api_path.dart';
 import 'package:nedaa/modules/prayer_times/models/prayer_times.dart';
 
-dynamic _parseApiResponse(response) {
-  // return DayPrayerTimes(
-  // );
-  return 1;
+dynamic _parseApiResponse(Map<String, dynamic> response) {
+  if (response['code'] == 200 && response['data'] is List) {
+    var all_days = response['data'].map((day) => DayPrayerTimes.fromJson(day));
+    // TODO: handle all days data
+    return;
+  }
+
+  throw Exception('Failed to parse post');
 }
 
 Future<http.Response> getPrayerTimes(String getParams) async {
   var response = await http.get(Uri.parse(getCalendar + '/?' + getParams));
-
   if (response.statusCode == 200) {
-    _parseApiResponse(response);
+    Map<String, dynamic> map = json.decode(response.body);
+    _parseApiResponse(map);
     return response;
   } else {
     throw Exception('Failed to get prayer times');
@@ -26,6 +29,8 @@ Future<http.Response> getPrayerTimesByCity(String getParams) async {
       await http.get(Uri.parse(getCalendarByCity + '/?' + getParams));
 
   if (response.statusCode == 200) {
+    Map<String, dynamic> map = json.decode(response.body);
+    _parseApiResponse(map);
     return response;
   } else {
     throw Exception('Failed to get prayer times');
