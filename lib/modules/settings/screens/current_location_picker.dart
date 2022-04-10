f58d8f2b5b170426a12d2d4597ecc80107b2c3ba
Fahad <42780409+F2had@users.noreply.github.com>
2022-04-10 06:53:09 +0300

    Fix flutter format making ci failing

--- lib/modules/settings/screens/current_location_picker.dart
+++ lib/modules/settings/screens/current_location_picker.dart
@@ -81,8 +81,8 @@ class _CurrentLocationPickerState extends State<CurrentLocationPicker> {
   _updateUserLocation() async {
     if ((cityValue.isNotEmpty || stateValue.isNotEmpty) &&
         countryValue.isNotEmpty) {
-      Future<List<Location>> locations =
-          locationFromAddress(cityValue + ', ' + stateValue + ', ' + countryValue);
+      Future<List<Location>> locations = locationFromAddress(
+          cityValue + ', ' + stateValue + ', ' + countryValue);
       Location location = await locations.then((value) => value[0]);
       context.read<UserSettingsBloc>().add(
             UserLocationEvent(
