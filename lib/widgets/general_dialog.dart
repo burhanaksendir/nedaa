f2e3853798e5bc793b51718d1064e387f3e68311
Fahad <42780409+F2had@users.noreply.github.com>
2022-04-21 06:09:43 +0300

    Edit custom alert to make showOk & showCancel as optional args

--- lib/widgets/general_dialog.dart
+++ lib/widgets/general_dialog.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-Future<bool> customAlert(
-    BuildContext context, String title, String content) async {
+Future<bool> customAlert(BuildContext context, String title, String content,
+    {bool showOk = true, bool showCancel = true}) async {
   var t = AppLocalizations.of(context);
-
   bool? ret = await showDialog(
       context: context,
       builder: (context) {
@@ -12,18 +11,22 @@ Future<bool> customAlert(
           title: Text(title),
           content: Text(content),
           actions: <Widget>[
-            TextButton(
-              child: Text(t!.ok),
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-            ),
-            TextButton(
-              child: Text(t.cancel),
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-            )
+            showOk
+                ? TextButton(
+                    child: Text(t!.ok),
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                    },
+                  )
+                : Container(),
+            showCancel
+                ? TextButton(
+                    child: Text(t!.cancel),
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                  )
+                : Container(),
           ],
         );
       });
